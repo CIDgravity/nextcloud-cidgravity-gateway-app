858998db843ea653a32ee842a9c03a5f645b8f05
Florian RUEN <florian.ruen@gmail.com>
2024-07-09 21:01:01 +0200

    fix: create folder on webdav external storage using curl requests

--- lib/Event/Listener/UserCreatedListener.php
+++ lib/Event/Listener/UserCreatedListener.php
@@ -32,6 +32,8 @@ use OCP\User\Events\UserCreatedEvent;
 use OCA\Files_External\Service\GlobalStoragesService;
 use OCP\Files\IRootFolder;
 
+use OCA\Files_External\Lib\StorageConfig;
+
 class UserCreatedListener implements IEventListener
 {
     public function __construct(private LoggerInterface $logger, private IConfig $config, private GlobalStoragesService $globalStoragesService, private IRootFolder $rootFolder) {}
@@ -44,7 +46,7 @@ class UserCreatedListener implements IEventListener
         $this->logger->debug("CIDgravity: will create the user folder on provided external storage");
 
         if (!($event instanceof UserCreatedEvent)) {
-            $this->logger->debug("CIDgravity: invalid event to create user folder on provided external storage");
+            $this->logger->warning("CIDgravity: invalid event to create user folder on provided external storage");
             return;
         }
 
@@ -54,27 +56,85 @@ class UserCreatedListener implements IEventListener
 
         foreach ($externalStorages as $externalStorage) {
             if ($externalStorage->getBackend()->getIdentifier() != "cidgravity") {
-                $this->logger->error("CIDgravity: unable to create user folder on provided external storage: not an cidgravity external storage");
-                return;
+                continue;
             }
 
             if ($externalStorage->getBackendOption('auto_create_user_folder')) {
-                $userUID = $event->getUser()->getUID();
-                $externalStorageMountpoint = $externalStorage->getMountPoint();
-                $userFolder = $this->rootFolder->getUserFolder($userUID);
-                $webDavPath = "/{$externalStorageMountpoint}/{$userUID}";
 
-                try {
-                    if (!$userFolder->nodeExists($webDavPath)) {
-                        $userFolder->newFolder($webDavPath);
-                        $this->logger->debug("CIDgravity: User folder successfully created on provided external storage");
-                    } else {
-                        $this->logger->debug("CIDgravity: unable to create user folder on provided external storage: already exists");
+                // here to resolve, we need to replace $user by userID
+                // we can't use userConfigHandler, because it works with userSession = current loggedin user
+                // here we want the created user, not the user current loggedin (due to event listener)
+                $resolvedMountpoint = str_replace('$user', $event->getUser()->getUID(), $externalStorage->getBackendOption('root'));
+                    
+                // define configuration to connect to webdav using CURL
+                $webDavPath = "{$externalStorage->getBackendOption('host')}/{$resolvedMountpoint}";
+
+                // check folder not already exists using PROPFIND in curl
+                $folderExists = $this->sendCurlRequestToWebdav($externalStorage, $webDavPath, 'PROPFIND');
+
+                if ($folderExists['errorMessage'] != null) {
+                    $this->logger->error("CIDgravity: unable to check if folder already exist on webdav server. Check full log for details", [
+                        "errorMessage" => $folderExists['errorMessage'],
+                        "httpStatusCode" => $folderExists['httpCode'],
+                    ]);
+                    
+                    return;
+                }
+
+                // if not exists (404 status code from previous request), create folder on webdav
+                if ($folderExists['httpCode'] == 404) {
+                    $createFolder = $this->sendCurlRequestToWebdav($externalStorage, $webDavPath, 'MKCOL');
+
+                    if ($createFolder['errorMessage'] != null) {
+                        $this->logger->error("CIDgravity: unable to create folder on webdav external storage. Check full log for details", [
+                            "errorMessage" => $createFolder['errorMessage'],
+                            "httpStatusCode" => $createFolder['httpCode'],
+                        ]);
+                        
+                        return;
                     }
-                } catch (\Exception $e) {
-                    $this->logger->error("CIDgravity: unable to create user folder on provided external storage: " . $e->getMessage());
-                }   
+
+                    $this->logger->debug("CIDgravity: user folder on external storage successfully created", [
+                        "webDavPath" => $webDavPath
+                    ]);
+
+                } else {
+                    $this->logger->debug("CIDgravity: user folder already exists on webdav external storage, not created", [
+                        "webDavPath" => $webDavPath
+                    ]);
+                }
             }
         }
+    }
+
+    public function sendCurlRequestToWebdav(StorageConfig $externalStorage, string $hostWithFolderPath, string $requestType) {
+        $ch = curl_init();
+
+        // define curl session opts
+        curl_setopt($ch, CURLOPT_URL, $hostWithFolderPath);
+        curl_setopt($ch, CURLOPT_USERPWD, "{$externalStorage->getBackendOption('user')}:{$externalStorage->getBackendOption('password')}");
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $requestType);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/xml',
+        ]);
+
+        // execute request
+        curl_exec($ch);
+
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $errorMessage = '';
+
+        if (curl_errno($ch)) {
+            $errorMessage = 'cURL Error: ' . curl_error($ch);
+        } else {
+            if ($httpCode == 405) {
+                $errorMessage = "request type $requestType not supported";
+            }
+        }
+
+        // close session and return
+        curl_close($ch);
+        return ['httpCode' => $httpCode, 'errorMessage' => $errorMessage];
     }
 }
